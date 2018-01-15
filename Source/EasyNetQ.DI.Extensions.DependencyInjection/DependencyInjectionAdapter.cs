8e234e2d32cc2e5b3458920a45fae531fa03bfb1
wtijsma <wiebe@tijsma.com>
2018-01-15 13:39:04 +0100

    Added support for Extensions.DependencyInjection

---
+++ Source/EasyNetQ.DI.Extensions.DependencyInjection/DependencyInjectionAdapter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EasyNetQ.DI
+{
+    public class DependencyInjectionAdapter : IContainer, IDisposable
+    {
+        readonly IServiceCollection serviceCollection;
+        ServiceProvider container;
+
+        public DependencyInjectionAdapter(IServiceCollection serviceCollection = null)
+        {
+            this.serviceCollection = serviceCollection ?? new ServiceCollection();
+            this.serviceCollection.AddSingleton(this);
+        }
+
+        public ServiceProvider Container 
+        { 
+            get => container;
+            set => container = value;
+        }
+
+        public IServiceRegister Register<TService>(Func<IServiceProvider, TService> serviceCreator) where TService : class
+        {
+            if (serviceCreator == null)  throw new ArgumentNullException("serviceCreator");
+            serviceCollection.AddSingleton<TService>(serviceCreator(this));
+            container = serviceCollection.BuildServiceProvider();
+            return this;
+        }
+
+        public IServiceRegister Register<TService, TImplementation>()
+            where TService : class
+            where TImplementation : class, TService
+        {
+            serviceCollection.AddSingleton<TService, TImplementation>();
+            container = serviceCollection.BuildServiceProvider();
+            return this;
+        }
+
+        public TService Resolve<TService>() where TService : class
+        {
+            return (TService) Container.GetService(typeof(TService));
+        }
+
+        public void Dispose()
+        {
+            container?.Dispose();
+        }
+    }
+}
